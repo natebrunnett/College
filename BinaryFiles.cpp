958627f0d9ca4004b1f0008531dced02fb3777ea
Nathan Brunnett <87023327+natebrunnett@users.noreply.github.com>
2022-05-07 23:15:04 +0200

    Update BinaryFiles.cpp

--- BinaryFiles.cpp
+++ BinaryFiles.cpp
@@ -22,8 +22,6 @@ From binary -> Morse Code -> English
 
 using namespace std;
 
-/*necessarey*/
-
 string ToHex(const string& s, bool upper_case)
     {
 	    ostringstream ret;
@@ -35,7 +33,6 @@ string ToHex(const string& s, bool upper_case)
 }
 
 
-// necessary
 void CBinaryFiles(string sfile, std::vector<std::string> & hexDec)
 {
     ifstream::pos_type size;
@@ -50,15 +47,10 @@ void CBinaryFiles(string sfile, std::vector<std::string> & hexDec)
         file.read(memblock, size);
         file.close();
 
-        //cout << "the complete file content is in memory" << endl;
-
         std::string tohexed = ToHex(std::string(memblock, memblock + size), true);
-        //std::cout << tohexed << std::endl;
 
         std::cout << "Hexadecimal translation successful!" << endl;
-
-
-        //store each translated hexadecimal in a vector of strings
+	    
         string temp;
         for (int a = 0; a < tohexed.length(); a++)
         {
@@ -78,11 +70,9 @@ void CBinaryFiles(string sfile, std::vector<std::string> & hexDec)
 
 void main()
 {
-    //hexadecimal string
     std::vector<std::string>hexDec;
 	CBinaryFiles("Morse.bin", hexDec);
     
-    //used to read each object in vector of hexadecimals
     int i = 0;
     char hexDecNum[10];
 
@@ -90,10 +80,6 @@ void main()
     
     for (int q = 0; q < hexDec.size(); q++)
     {
-        //cout << "hexDec.at(" << q << ") = " << hexDec.at(q) << endl;
-        
-
-        //clear the char[] before use
         for (int a = 0; a < 10; a++)
         {
             hexDecNum[a] = NULL;
@@ -101,90 +87,63 @@ void main()
 
         strcpy(hexDecNum, hexDec.at(q).c_str());
 
-        /*
-        
-        for (int a = 0; a < 10; a++)
-        {
-            std::cout << hexDecNum[a];
-        }
-
-        */ 
-
-       // std::cout << "Equivalent Binary Value = ";
         i = 0;
         while (hexDecNum[i])
         {
             switch (hexDecNum[i])
             {
             case '0':
-                //cout << "0000";
                 bits += "0000";
                 break;
             case '1':
-               // cout << "0001";
                 bits += "0001";
                 break;
             case '2':
-              //  cout << "0010";
                 bits += "0010";
                 break;
             case '3':
-               // cout << "0011";
                 bits += "0011";
                 break;
             case '4':
-               // cout << "0100";
                 bits += "0100";
                 break;
             case '5':
-               // cout << "0101";
                 bits += "0101";
                 break;
             case '6':
-                //cout << "0110";
                 bits += "0110";
                 break;
             case '7':
-               // cout << "0111";
                 bits += "0111";
                 break;
             case '8':
-                //cout << "1000";
                 bits += "1000";
                 break;
             case '9':
-               // cout << "1001";
                 bits += "1001";
                 break;
             case 'A':
             case 'a':
-               // cout << "1010";
                 bits += "1010";
                 break;
             case 'B':
             case 'b':
-               // cout << "1011";
                 bits += "1011";
                 break;
             case 'C':
             case 'c':
-              //  cout << "1100";
                 bits += "1100";
                 break;
             case 'D':
             case 'd':
-             //   cout << "1101";
                 bits += "1101";
                 break;
             case 'E':
             case 'e':
-            //    cout << "1110";
                 bits += "1110";
                 break;
             case 'F':
             case 'f':
-                //error prone,
-                //cout << "1111";
                 break;
             default:
                 cout << "--Invalid Hex Digit (" << hexDecNum[i] << ")--";
@@ -194,7 +153,6 @@ void main()
     }
 
     std::cout << "Hexadecimal to Binary successful " << endl;
-    //std::cout << "Bits = " << bits << endl;
 
 
     std::vector<char>decrypted;
@@ -207,25 +165,19 @@ void main()
 
         if (binary == "00")
         {
-            //do nothing
-            //cout << binary << " = letter space" << endl;
             decrypted.push_back('L');
-            //L for letter space
         }
         else if (binary == "01")
         {
             decrypted.push_back('-');
-            //cout << binary << " = -" << endl;
         }
         else if (binary == "10")
         {
             decrypted.push_back('.');
-            //cout << binary << " = ." << endl;
         }
         else if (binary == "11")
         {
             decrypted.push_back(' ');
-           // cout << binary << " = \' \'" << endl;
         }
        
     }
@@ -258,7 +210,6 @@ void main()
         if (decrypted.at(i) == 'L')
         {
             currentChar.pop_back();
-            //std::cout << "currentChar = " << currentChar << endl;
             if (currentChar == ".-")
                 message += "A";
             if (currentChar == "-...")
